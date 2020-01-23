7d53e5b2b00e73c7be59c7aef62c62460fff705b
kpacha <kpacha666@gmail.com>
2020-01-23 18:47:38 +0100

    moving to go modules

--- gin/jose_example_test.go
+++ gin/jose_example_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"time"
 
 	krakendjose "github.com/devopsfaith/krakend-jose"
@@ -32,16 +33,19 @@ func Example_RS256() {
 	// token request
 	// 201
 	// {"access_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6IjIwMTEtMDQtMjkifQ.eyJhdWQiOiJodHRwOi8vYXBpLmV4YW1wbGUuY29tIiwiZXhwIjoxNzM1Njg5NjAwLCJpc3MiOiJodHRwOi8vZXhhbXBsZS5jb20iLCJqdGkiOiJtbmIyM3Zjc3J0NzU2eXVpb21uYnZjeDk4ZXJ0eXVpb3AiLCJyb2xlcyI6WyJyb2xlX2EiLCJyb2xlX2IiXSwic3ViIjoiMTIzNDU2Nzg5MHF3ZXJ0eXVpbyJ9.NrLwxZK8UhS6CV2ijdJLUfAinpjBn5_uliZCdzQ7v-Dc8lcv1AQA9cYsG63RseKWH9u6-TqPKMZQ56WfhqL028BLDdQCiaeuBoLzYU1tQLakA1V0YmouuEVixWLzueVaQhyGx-iKuiuFhzHWZSqFqSehiyzI9fb5O6Gcc2L6rMEoxQMaJomVS93h-t013MNq3ADLWTXRaO-negydqax_WmzlVWp_RDroR0s5J2L2klgmBXVwh6SYy5vg7RrnuN3S8g4oSicJIi9NgnG-dDikuaOg2DeFUt-mYq_j_PbNXf9TUl5hl4kEy7E0JauJ17d1BUuTl3ChY4BOmhQYRN0dYg","exp":1735689600,"refresh_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6IjIwMTEtMDQtMjkifQ.eyJhdWQiOiJodHRwOi8vYXBpLmV4YW1wbGUuY29tIiwiZXhwIjoxNzM1Njg5NjAwLCJpc3MiOiJodHRwOi8vZXhhbXBsZS5jb20iLCJqdGkiOiJtbmIyM3Zjc3J0NzU2eXVpb21uMTI4NzZidmN4OThlcnR5dWlvcCIsInN1YiI6IjEyMzQ1Njc4OTBxd2VydHl1aW8ifQ.v5dzeXlcYGOCwlhJ05tQ7JXgNw_KO49YvAtURxUOlWqF-OMExzjbevNPSZ2tdWrf8FO5VByoLW6b4cD_6-4PS5XAvTcip2GHOLsvfBokCaxRcMc-tSF-wfPQ4Z2B2GM3_0ErmXC5bSTuBeGaYQ76dONKFUDn7t2lxuABD9oEsLfQYJDnzhCkOzBo8Gg_AY1Vyx-MEYIcatqHI52QGi2_6EBbpJ2ienOaoeGgMfrOMWKFAmBABLkxjnNCzEjAR2lT04NWdB4NnXNa3-m8WedF2TZzmcWzp3mtI9uJhMjpnu8rNi1Uy8LAm6qCjVZABtgfLs-YZekQ2JXx_b0Zojg7og"}
+	//
 	// map[Content-Type:[application/json; charset=utf-8]]
 	// unauthorized request
 	// 401
 	// authorized request
 	// 200
 	// {}
+	//
 	// [application/json; charset=utf-8]
 	// dummy request
 	// 200
 	// {}
+	//
 	// [application/json; charset=utf-8]
 	//  INFO: JOSE: singer disabled for the endpoint /private
 	//  INFO: JOSE: validator enabled for the endpoint /private
@@ -63,16 +67,19 @@ func Example_HS256() {
 	// token request
 	// 201
 	// {"access_token":"eyJhbGciOiJIUzI1NiIsImtpZCI6InNpbTIifQ.eyJhdWQiOiJodHRwOi8vYXBpLmV4YW1wbGUuY29tIiwiZXhwIjoxNzM1Njg5NjAwLCJpc3MiOiJodHRwOi8vZXhhbXBsZS5jb20iLCJqdGkiOiJtbmIyM3Zjc3J0NzU2eXVpb21uYnZjeDk4ZXJ0eXVpb3AiLCJyb2xlcyI6WyJyb2xlX2EiLCJyb2xlX2IiXSwic3ViIjoiMTIzNDU2Nzg5MHF3ZXJ0eXVpbyJ9.vTdN1Nm6Eeb3oJWC5yOpmvwTrwuXFYkqy2131u3G0Hk","exp":1735689600,"refresh_token":"eyJhbGciOiJIUzI1NiIsImtpZCI6InNpbTIifQ.eyJhdWQiOiJodHRwOi8vYXBpLmV4YW1wbGUuY29tIiwiZXhwIjoxNzM1Njg5NjAwLCJpc3MiOiJodHRwOi8vZXhhbXBsZS5jb20iLCJqdGkiOiJtbmIyM3Zjc3J0NzU2eXVpb21uMTI4NzZidmN4OThlcnR5dWlvcCIsInN1YiI6IjEyMzQ1Njc4OTBxd2VydHl1aW8ifQ.F7KWdUacMQX9g2SGk-UMAU0kfC4xUFsuB-QTFdg9P-M"}
+	//
 	// map[Content-Type:[application/json; charset=utf-8]]
 	// unauthorized request
 	// 401
 	// authorized request
 	// 200
 	// {}
+	//
 	// [application/json; charset=utf-8]
 	// dummy request
 	// 200
 	// {}
+	//
 	// [application/json; charset=utf-8]
 	//  INFO: JOSE: singer disabled for the endpoint /private
 	//  INFO: JOSE: validator enabled for the endpoint /private
@@ -132,11 +139,12 @@ func Example_HS256_cookie() {
 	fmt.Println(w.Body.String())
 	fmt.Println(w.HeaderMap["Content-Type"])
 
-	fmt.Println(buf.String())
+	printLog(buf)
 
 	// output:
 	// 200
 	// {}
+	//
 	// [application/json; charset=utf-8]
 	//  INFO: JOSE: singer disabled for the endpoint /private
 	//  INFO: JOSE: validator enabled for the endpoint /private
@@ -207,7 +215,7 @@ func runValidationCycle(signerEndpointCfg, validatorEndpointCfg *config.Endpoint
 	fmt.Println(w.Body.String())
 	fmt.Println(w.HeaderMap["Content-Type"])
 
-	fmt.Println(buf.String())
+	printLog(buf)
 }
 
 func tokenIssuer(ctx context.Context, req *proxy.Request) (*proxy.Response, error) {
@@ -284,5 +292,15 @@ func newVerifierEndpointCfg(alg, URL string, roles []string) *config.EndpointCon
 				"cache":                true,
 			},
 		},
+	}
+}
+
+func printLog(buf *bytes.Buffer) {
+	for _, l := range strings.Split(buf.String(), "\n") {
+		if len(l) <= 20 {
+			fmt.Println(l)
+			continue
+		}
+		fmt.Println(l[20:])
 	}
 }
