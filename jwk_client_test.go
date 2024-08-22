8d661436a4a144e8fba1994fc230a957d38586da
kpacha <kpacha666@gmail.com>
2024-08-22 19:24:59 +0200

    ignore unused param

--- jwk_client_test.go
+++ jwk_client_test.go
@@ -23,7 +23,7 @@ func TestJWKClient_globalCache(t *testing.T) {
 	}]}`)
 
 	var count uint64
-	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		// Content-Type defined in https://datatracker.ietf.org/doc/html/rfc7517#section-8.5.1
 		w.Header().Add("Content-Type", "application/jwk-set+json")
 		atomic.AddUint64(&count, 1)
